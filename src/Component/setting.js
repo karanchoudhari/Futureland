1b4beef3fc91ca6c41692e4797169f375f354972
adityapanday <aditya.panday.1986.ad@gmail.com>
2025-03-18 11:46:44 +0530

    pulling

--- src/Component/setting.js
+++ src/Component/setting.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, reset } from "../FeatureRedux/testReducer/testReducer";
 
-const setting = () => {
+const Setting = () => {  
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
   return (
-   <>
-   
-   </>
-  )
-}
+    <>
+      <div>
+        <h2>Count: {count}</h2>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+        <button onClick={() => dispatch(reset())}>Reset</button>
+      </div>
+    </>
+  );
+};
 
-export default setting
+export default Setting; // ✅ Also ensure it's exported as "Setting"
--- src/index.js
+++ src/index.js
@@ -1,6 +1,116 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'; 
+// import App from './App';
+// import './index.css';  // Import global styles
+// import Dashboardindex from './Component/Dashboard/dashboardindex';
+// import Setting from './Component/setting';
+// import Management from './Component/Management/management';
+// import Chart from './Component/Overview/chart';
+// import Overview from './Component/Overview/overview';
+// import Addproject from './Component/Projectsfolder/addproject';
+// import Table3 from './Component/Table/table3';
+// import Dashboard from './Component/Dashboard/dashboard';
+// import Detail from './Component/Table/detail';
+// import ProjectList from './Component/Projectsfolder/projectlist';
+// import AddProject1 from './Component/Projectsfolder/addProject1';
+// import UserProfile from './Component/UserProfile/userprofile';
+// import Login from './Component/Loginpage/login';
+// import Addblogs  from './Component/Management/Blog/blogadd'
+// import Addgraph from './Component/Overview/addgraph';
+// import Blogfront from './Component/Management/Blog/blogfront';
+// import store from './store'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     children: [
+//       {
+//         path: '/dashboard',
+//         element: <Dashboardindex/>,
+       
+      
+//       },
+//       {path:'/management', element:<Management/>},
+//       { path:'/detail',
+//         element:<Detail/>},
+//         { path:'/user' , element:<UserProfile/>},
+//       {
+//         path:'/settings',
+//         element:<Setting/>,
+//       },
+//       {
+//         path:'/login',
+//         element:<Login/>,
+//       },
+//       {
+//         path:'/add-blogs',
+//         element:<Addblogs/>,
+//       },
+//       {
+//         path:'/Blogfront',
+//         element:<Blogfront/>,
+//       },
+//       {
+//         path:'/addgraph',
+//         element:<Addgraph/>,
+//       },
+  
+//       {
+//         path:'/chart',
+//         element:<Chart/>,
+//       },
+//       {
+//         path:'/overview',
+//         element:<Overview/>,
+//       },
+//       {
+//         path:'/addproject',
+//         element:<Addproject/>,
+//       },
+//       {
+//         path:'/addProject1',
+//         element:<AddProject1/>
+//       },
+//       {
+//         path:'/projectlist',
+//         element:<ProjectList/>,
+//       },
+   
+//       {
+//         path:'/table3',
+//         element:<Table3/>,
+//       },
+//       {
+//         path:'/dash',
+//         element:<Dashboard/>,
+//       },
+//     ],
+//   },
+
+//   // {
+//   //   path: '/detail',
+//   //   element: <>detail</>,
+//   // },
+// ]);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
+
+
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'; 
+import { Provider } from 'react-redux';  // Import Provider from react-redux
+import store from './store';  // Import the Redux store
 import App from './App';
 import './index.css';  // Import global styles
 import Dashboardindex from './Component/Dashboard/dashboardindex';
@@ -16,86 +126,40 @@ import ProjectList from './Component/Projectsfolder/projectlist';
 import AddProject1 from './Component/Projectsfolder/addProject1';
 import UserProfile from './Component/UserProfile/userprofile';
 import Login from './Component/Loginpage/login';
-import Addblogs  from './Component/Management/Blog/blogadd'
+import Addblogs from './Component/Management/Blog/blogadd';
 import Addgraph from './Component/Overview/addgraph';
 import Blogfront from './Component/Management/Blog/blogfront';
+
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     children: [
-      {
-        path: '/dashboard',
-        element: <Dashboardindex/>,
-       
-      
-      },
-      {path:'/management', element:<Management/>},
-      { path:'/detail',
-        element:<Detail/>},
-        { path:'/user' , element:<UserProfile/>},
-      {
-        path:'/settings',
-        element:<Setting/>,
-      },
-      {
-        path:'/login',
-        element:<Login/>,
-      },
-      {
-        path:'/add-blogs',
-        element:<Addblogs/>,
-      },
-      {
-        path:'/Blogfront',
-        element:<Blogfront/>,
-      },
-      {
-        path:'/addgraph',
-        element:<Addgraph/>,
-      },
-  
-      {
-        path:'/chart',
-        element:<Chart/>,
-      },
-      {
-        path:'/overview',
-        element:<Overview/>,
-      },
-      {
-        path:'/addproject',
-        element:<Addproject/>,
-      },
-      {
-        path:'/addProject1',
-        element:<AddProject1/>
-      },
-      {
-        path:'/projectlist',
-        element:<ProjectList/>,
-      },
-   
-      {
-        path:'/table3',
-        element:<Table3/>,
-      },
-      {
-        path:'/dash',
-        element:<Dashboard/>,
-      },
+      { path: '/dashboard', element: <Dashboardindex /> },
+      { path: '/management', element: <Management /> },
+      { path: '/detail', element: <Detail /> },
+      { path: '/user', element: <UserProfile /> },
+      { path: '/settings', element: <Setting /> },
+      { path: '/login', element: <Login /> },
+      { path: '/add-blogs', element: <Addblogs /> },
+      { path: '/Blogfront', element: <Blogfront /> },
+      { path: '/addgraph', element: <Addgraph /> },
+      { path: '/chart', element: <Chart /> },
+      { path: '/overview', element: <Overview /> },
+      { path: '/addproject', element: <Addproject /> },
+      { path: '/addProject1', element: <AddProject1 /> },
+      { path: '/projectlist', element: <ProjectList /> },
+      { path: '/table3', element: <Table3 /> },
+      { path: '/dash', element: <Dashboard /> },
     ],
   },
-
-  // {
-  //   path: '/detail',
-  //   element: <>detail</>,
-  // },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>  {/* Wrap everything with Provider */}
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
