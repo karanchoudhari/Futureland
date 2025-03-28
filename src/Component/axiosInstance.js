163af31ea24b068bcf464c2a5b920ea01e7e5f8a
adityapanday <aditya.panday.1986.ad@gmail.com>
2025-03-28 17:05:31 +0530

    pushing

--- src/Component/axiosInstance.js
+++ src/Component/axiosInstance.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // Base URL dynamically set hota hai environment variables se
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3002/api'
-  // baseURL: process.env.REACT_APP_API_BASE_URL || 'http://13.126.84.252:80/api'
+  // baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3002/api'
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://13.126.84.252:80/api'
   , // Environment variable
   timeout: 5000, // Optional: Timeout 5 seconds
   headers: {
